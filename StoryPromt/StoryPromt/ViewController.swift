2d7a88348f5f10d82f34df8c7cf9aea868a8b6e6
곽붕기 <boongkk1991@gmail.com>
2022-08-25 01:01:26 +0900

    FEAT: StoryBoard Update

--- StoryPromt/StoryPromt/ViewController.swift
+++ StoryPromt/StoryPromt/ViewController.swift
@@ -8,15 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UIStackView!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    
+    let storyPrompt = StoryPromptEntry()
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex){
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPromt = StoryPromptEntry()
-        storyPromt.noun = "toaster"
-        storyPromt.adjective = "smelly"
-        storyPromt.verb = "burps"
-        storyPromt.number = 10
-        print(storyPromt)
+        
+        numberSlider.value = 7.5
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = Int(numberSlider.value)
+        print(storyPrompt)
     }
 
 
