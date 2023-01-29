3f2306ec7622c4642817d48a7f26e3fd2450fa25
alex99091 <bkk99091@gmail.com>
2023-01-29 20:16:46 +0900

    updated

---
+++ Bullseye/Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by BOONGKI KWAK on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
